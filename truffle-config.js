e239229967de5ca75e37ed6e991f0697620df6a6
marvinkruse <marvinkruse@users.noreply.github.com>
2021-01-21 13:05:59 +0000

    Updated dependencies

--- truffle-config.js
+++ truffle-config.js
@@ -3,37 +3,38 @@ const HDWalletProvider = require("@truffle/hdwallet-provider");
 module.exports = {
   networks: {
     development: {
-     host: "127.0.0.1",
-     port: 8545,
-     gasPrice: 1*1000000000,
-     gasLimit: 12000000,
-     network_id: "*",
+      host: "127.0.0.1",
+      port: 8545,
+      gasPrice: 1 * 1000000000,
+      gasLimit: 10000000,
+      network_id: "*",
     },
     rinkeby: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, "https://rinkeby.infura.io/v3/" + process.env.INFURA_KEY),
-      gasPrice: 1*1000000000,
+      gasPrice: 5 * 1000000000,
+      gasLimit: 10000000,
       network_id: 4,
       skipDryRun: true,
     },
     sokol: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, "https://sokol.poa.network"),
-      gasPrice: 1*1000000000,
+      gasPrice: 1 * 1000000000,
       network_id: 77,
       skipDryRun: true,
     },
     xdai: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, "https://rpc.xdaichain.com/"),
-      gasPrice: 1*1000000000,
+      gasPrice: 1 * 1000000000,
       network_id: 100,
     },
     matic: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, "https://rpc-mainnet.matic.network"),
-      gasPrice: 1*1000000000,
+      gasPrice: 1 * 1000000000,
       network_id: 137,
     },
     mainnet: {
       provider: () => new HDWalletProvider(process.env.PRIVATE_KEY, "https://infura.io/v3/" + process.env.INFURA_KEY),
-      gasPrice: 1*1000000000,
+      gasPrice: 1 * 1000000000,
       network_id: 4,
     },
   },
@@ -53,7 +54,7 @@ module.exports = {
   plugins: [
     'truffle-source-verify'
   ],
-  
+
   api_keys: {
     etherscan: process.env.ETHERSCAN_KEY
   }
