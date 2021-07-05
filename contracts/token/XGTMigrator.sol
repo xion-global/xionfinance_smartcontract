23f10b7fd79c8dbf421cd325bcc2c18aa8d68846
marvinkruse <marvinkruse@users.noreply.github.com>
2021-07-05 07:47:42 +0000

    Add price changes for exchange rate

--- contracts/token/XGTMigrator.sol
+++ contracts/token/XGTMigrator.sol
@@ -21,8 +21,9 @@ contract XGTMigrator {
     IUniswapV2Router02 public newRouter;
     address public newPool;
 
-    uint256 exchangeRate = 200;
-    uint256 startTime = 0; // TODO set time correctly after it's known
+    uint256 public exchangeRate = 200;
+    uint256 public lastPriceV1 = 0;
+    uint256 public startTime = 0; // TODO set time correctly after it's known
 
     constructor(
         address _oldToken,
@@ -210,7 +211,12 @@ contract XGTMigrator {
 
         // XGT TOKEN
         // Check whether user has XGT v1
-        uint256 migrationAmountXGT = oldToken.balanceOf(_from);
+        uint256 migrationAmountXGT =
+            oldToken.allowance(msg.sender, address(this));
+        uint256 balanceXGT = oldToken.balanceOf(msg.sender);
+        if (balanceXGT < migrationAmountXGT) {
+            migrationAmountXGT = balanceXGT;
+        }
 
         // If user has v1, transfer them here
         if (migrationAmountXGT > 0) {
@@ -229,6 +235,21 @@ contract XGTMigrator {
             newToken.transfer(_to, finalReturnXGT),
             "MIGRATOR-TRANSFER-NEW-TOKEN-FAILED"
         );
+    }
+
+    function setLastPriceV1(uint256 _lastPriceV1) external onlyController {
+        // for the input
+        // e.g. $0.15 per XGT would be 150000000000000000 (0.15 * 10^18)
+        lastPriceV1 = _lastPriceV1;
+    }
+
+    function updateExchangeRate(uint256 _currentPriceV2)
+        external
+        onlyController
+    {
+        // for the input
+        // e.g. $0.20 per XGT would be 200000000000000000 (0.2 * 10^18)
+        exchangeRate = (lastPriceV1.mul(1000)).div(_currentPriceV2);
     }
 
     modifier onlyController() {
